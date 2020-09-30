6d3956184f87923b429014715b188ccf5583714a
Melissa McEwen <mgmcewen@gmail.com>
2020-09-30 14:53:39 -0500

    Create vue.js

---
+++ vue.js
@@ -0,0 +1,53 @@
+---
+url: https://vuejs.org/v2/guide/
+---
+
+# Essentials
+- Installation
+- Introduction
+- The Vue Instance
+- Template Syntax
+- Computed Properties and Watchers
+- Class and Style Bindings
+- Conditional Rendering
+- List Rendering
+- Event Handling
+- Form Input Bindings
+- Components Basics
+# Components In-Depth
+- Component Registration
+- Props
+- Custom Events
+- Slots
+- Dynamic & Async Components
+- Handling Edge Cases
+# Transitions & Animation
+- Enter/Leave & List Transitions
+- State Transitions
+# Reusability & Composition
+- Mixins
+- Custom Directives
+- Render Functions & JSX
+- Plugins
+- Filters
+# Tooling
+- Single File Components
+- Testing
+- TypeScript Support
+- Production Deployment
+# Scaling Up
+- Routing
+- State Management
+- Server-Side Rendering
+- Security
+# Internals
+- Reactivity in Depth
+# Migrating
+- Migration from Vue 1.x
+- Migration from Vue Router 0.7x
+- Migration from Vuex 0.6 to 1.0
+# Meta
+- Comparison with Other Frameworks
+- Joins the Vue.js Community
+- Meet the Team
+# Meta
